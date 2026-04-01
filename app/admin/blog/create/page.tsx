6fb6f24138b8afb91c13937071f3438cfccb1281
Yahya Shahzad <your-email@gmail.com>
2026-04-01 10:14:05 -0400

    fix all dynamic prisma pages

--- app/admin/blog/create/page.tsx
+++ app/admin/blog/create/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+export const dynamic = "force-dynamic";
 import { useState } from "react";
 import Editor from "@/components/Editor";
 
--- app/api/blog/create/route.ts
+++ app/api/blog/create/route.ts
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import prisma from "@/libs/prisma";
 
 export async function POST(req: Request) {
