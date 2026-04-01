bcd19e49803abb31d24022b340c8ba26ee106f83
Yahya Shahzad <your-email@gmail.com>
2026-04-01 11:04:31 -0400

    blog data added

--- libs/prisma.ts
+++ libs/prisma.ts
@@ -1,17 +1,16 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as typeof globalThis & {
-  prisma?: PrismaClient;
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
 };
 
-const prisma =
-  globalForPrisma.prisma ??
+export const prisma =
+  globalForPrisma.prisma ||
   new PrismaClient({
     log: ["query"],
   });
 
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== "production")
   globalForPrisma.prisma = prisma;
-}
 
 export default prisma;
